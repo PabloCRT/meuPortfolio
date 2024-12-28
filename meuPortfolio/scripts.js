73d8d57d99c620798a8bb9722238cd19eb390487
Pablo Silva <pablooom.silva@gmail.com>
2024-12-28 01:08:12 -0300

    Update scripts.js

--- meuPortfolio/scripts.js
+++ meuPortfolio/scripts.js
@@ -1,16 +1,26 @@
+let paginaRecarregada = false;
+
 window.addEventListener('scroll', function(){
     const elemento = document.getElementById('linkInt');
+    const contrato = this.document.getElementById('contrate');
 
     if(this.window.scrollY === 0){
         elemento.style.opacity="0"
         elemento.style.transition=".5s ease"
+
+        contrato.style.opacity="1"
     }
 
     else{
         elemento.style.opacity="1"
         elemento.style.transition=".5s ease"
+        
+        contrato.style.opacity="0"
+        contrato.style.transition=".5s ease"
     }
 });
+
+
 
 window.addEventListener('scroll', function(){
     const navBar = this.document.getElementById('navBar');
@@ -20,11 +30,11 @@ window.addEventListener('scroll', function(){
         navBar.style.boxShadow="0px 0px 0px"
     }
     else{
-        navBar.style.backgroundColor="rgb(196, 208, 219)"
+        navBar.style.backgroundColor="rgb(8, 8, 8)"
         navBar.style.zIndex="1001"
         navBar.style.height="10vh"
         navBar.style.transition="ease .5s"
-        navBar.style.boxShadow="0px 5px 10px rgb(171, 182, 192)"
+        navBar.style.boxShadow="0px 5px 10px black"
     }
 });
 
