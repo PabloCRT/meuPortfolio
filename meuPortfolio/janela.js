04b0299404981c739a8db2cf6e31fbd1829ad761
Pablo Silva <pablooom.silva@gmail.com>
2024-12-28 01:08:47 -0300

    Add files via upload

---
+++ meuPortfolio/janela.js
@@ -0,0 +1,8 @@
+/** Parte das linguagens. Esse código faz com que uma janela se abra a partir do onclick de um elemento (INÍCIO) */
+function janela(){
+    const imagem = document.getElementById('imgGhost').src = 'assets/css3.png';
+    
+    imagem.style.display="block";
+}
+/** Parte das linguagens. Esse código faz com que uma janela se abra a partir do onclick de um elemento (FIM) */
+
