6c80f4d00225e5696ec7cbc10cd2995d84f0bcb0
Pablo Silva <pablooom.silva@gmail.com>
2024-12-29 01:27:17 -0300

    Delete meuPortfolio directory

--- meuPortfolio/janela.js
+++
@@ -1,8 +0,0 @@
-/** Parte das linguagens. Esse código faz com que uma janela se abra a partir do onclick de um elemento (INÍCIO) */
-function janela(){
-    const imagem = document.getElementById('imgGhost').src = 'assets/css3.png';
-    
-    imagem.style.display="block";
-}
-/** Parte das linguagens. Esse código faz com que uma janela se abra a partir do onclick de um elemento (FIM) */
-
