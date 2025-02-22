3cd19606576d3c9717db85d9d9b4740d69060bad
Pablo Silva <pablooom.silva@gmail.com>
2025-02-22 01:48:35 -0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,6 @@
+function navegacao(){
+    let contatos = document.getElementById("contatos");
+
+    contatos.style.background="rgba(0, 2, 15, 0.432)"    /**Destaca a seção dos contatos/redes alterando sua cor de fundo */
+    contatos.style.transition="2s all"
+}
