e92f343b52a6abf2a0b56ee18cdb8b9a2dcab84e
Pablo Silva <pablooom.silva@gmail.com>
2025-03-07 23:48:19 -0300

    Update nav.js

--- JS/nav.js
+++ JS/nav.js
@@ -41,3 +41,9 @@ function fecharAposClick(){
 
     botao.setAttribute('onclick', "abrirMenu()")
 }
+
+function navegacao(){
+    const redes = document.getElementById('contatos');
+
+    redes.style.background="black"
+}
