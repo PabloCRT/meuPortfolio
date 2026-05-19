a3b33b139a77a0ef884b48427d62aaa081c28025
Pablo Silva <pablooom.silva@gmail.com>
2026-05-19 19:39:40 -0300

    Correção script scroll
    Adicionei um querySelectorAll para selecionar todas as classes iguais por meio do forEach

--- JS/scroll.js
+++ JS/scroll.js
@@ -2,14 +2,16 @@
 //Não terminado...
 
 window.addEventListener('scroll', () => {
-    const elementoObservado = document.querySelector('.containerMaisProjetos');
+    const elementoObservado = document.querySelectorAll('.esquerda');
 
-    if(elementoObservado){
-        const gatilho = window.innerHeight / 2;
-        const distanciaTopo = elementoObservado.getBoundingClientRect().top;
-
-        if(distanciaTopo < gatilho){
-            elementoObservado.classList.add('projeto-esquerda-view');
+    forEach((el) => (elementoObsservado) {
+        if(elementoObservado){
+            const gatilho = window.innerHeight / 2;
+            const distanciaTopo = elementoObservado.getBoundingClientRect().top;
+    
+            if(distanciaTopo < gatilho){
+                elementoObservado.classList.add('projeto-esquerda-view');
+            }
         }
-    }
+    });
 })
